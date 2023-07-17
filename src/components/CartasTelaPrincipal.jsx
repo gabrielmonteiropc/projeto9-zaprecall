763c1ffd0d094ef6b37b305316f4105589e5d2f9
Gabriel <bhcmonteiro54@gmail.com>
2023-07-17 12:32:32 -0300

    data-teste

--- src/components/CartasTelaPrincipal.jsx
+++ src/components/CartasTelaPrincipal.jsx
@@ -46,7 +46,7 @@ export default function CartasTelaPrincipal(props) {
                 )
                 :
                 (
-                    < PerguntasA>
+                    < PerguntasA data-test="flashcard">
                         {!virarCarta
                             ?
                             (
